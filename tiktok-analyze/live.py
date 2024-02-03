70f57670c8a7dfa586f2cae9805f70e80874480b
cancan007 <shota.moue0211@gmail.com>
2024-02-03 17:31:02 +0900

    improved the code of tiktok-live sc

--- tiktok-analyze/live.py
+++ tiktok-analyze/live.py
@@ -5,6 +5,14 @@ from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 import datetime
 import os
+import time
+
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams["legend.framealpha"] = 1
+plt.rcParams["font.family"] = 'Times New Roman'
+plt.rcParams["mathtext.fontset"] = 'cm'
+plt.rcParams["font.size"] = 13
 
 dt_now = datetime.datetime.now()
 dt_today=f"{dt_now.year}-{dt_now.month}-{dt_now.day}"
@@ -53,7 +61,7 @@ def divide_time_area(t):
 def get_plus_by_viewers(df):
     if(df['total_viewers']==0):
         return 0
-    return df['plus_followers']/df['total_viewers']*100
+    return df['plus_followers']/df['total_viewers']
 
 def get_plus_by_time(df):
     if(df['time']==0):
@@ -71,74 +79,48 @@ data['total_viewers']=list(map(lambda x:int(x.replace(',','')),data['total_viewe
 #data['plus_followers']=list(map(lambda x:int(x.replace(',','')),data['plus_followers']))
 data['reward']=list(map(lambda x:int(x.replace(',','')),data['reward']))
 data['time']=list(map(calc_time, data['time']))
-data['plus_by_viewers[%]'] = data.apply(get_plus_by_viewers, axis=1)
+data['plus_by_viewers'] = data.apply(get_plus_by_viewers, axis=1)
 data['plus_by_time'] = data.apply(get_plus_by_time, axis=1)
 data=data.drop(['title','Unnamed: 0','start_date_time'],axis=1)
 corr=data.corr() #各項目の相関度
 print(corr)
 print(np.mean(data['plus_followers'])) 
 
-#四分位数でカラーわけ
-percentile=np.percentile(data['plus_followers'], q=[0, 25, 50, 75])
-data.loc[data['plus_followers'] > percentile[3] ,'color'] = 'blue'
-data.loc[(data['plus_followers'] <= percentile[3]) & (data['plus_followers'] > percentile[2]) ,'color'] = 'green'
-data.loc[(data['plus_followers'] <= percentile[2]) & (data['plus_followers'] > percentile[1]) ,'color'] = 'darkorange'
-data.loc[(data['plus_followers'] <= percentile[1]) ,'color'] = 'red'
-#data.loc[data['plus_followers'] <= percentile[0] ,'color'] = 'black'
-color=data['color']
-del data['color']
-print(data)
-plt.figure()
-plot=scatter_matrix(data,diagonal='density',color=color,figsize=(11,11))
-plt.suptitle(f'{dt_today}_plus_follwers')
-plt.savefig(f'{dt_today}/scatter_matrix_color_plus.png')
-
-#四分位数でカラーわけ
-percentile=np.percentile(data['plus_by_viewers[%]'], q=[0, 25, 50, 75])
-data.loc[data['plus_by_viewers[%]'] > percentile[3] ,'color'] = 'blue'
-data.loc[(data['plus_by_viewers[%]'] <= percentile[3]) & (data['plus_by_viewers[%]'] > percentile[2]) ,'color'] = 'green'
-data.loc[(data['plus_by_viewers[%]'] <= percentile[2]) & (data['plus_by_viewers[%]'] > percentile[1]) ,'color'] = 'darkorange'
-data.loc[(data['plus_by_viewers[%]'] <= percentile[1])  ,'color'] = 'red'
-#data.loc[data['plus_by_viewers[%]'] <= percentile[0] ,'color'] = 'black'
-color=data['color']
-del data['color']
-print(data)
-plt.figure()
-plot=scatter_matrix(data,diagonal='density',color=color,figsize=(11,11))
-plt.suptitle(f'{dt_today}_plus_by_viewers[%]')
-plt.savefig(f'{dt_today}/scatter_matrix_color_plus_by_viewers.png')
-
-#四分位数でカラーわけ
-percentile=np.percentile(data['plus_by_time'], q=[0, 25, 50, 75])
-data.loc[data['plus_by_time'] > percentile[3] ,'color'] = 'blue'
-data.loc[(data['plus_by_time'] <= percentile[3]) & (data['plus_by_time'] > percentile[2]) ,'color'] = 'green'
-data.loc[(data['plus_by_time'] <= percentile[2]) & (data['plus_by_time'] > percentile[1]) ,'color'] = 'darkorange'
-data.loc[(data['plus_by_time'] <= percentile[1]) ,'color'] = 'red'
-#data.loc[data['plus_by_time'] <= percentile[0] ,'color'] = 'black'
-color=data['color']
-del data['color']
-print(data)
-plt.figure()
-plot=scatter_matrix(data,diagonal='density',color=color,figsize=(11,11))
-plt.suptitle(f'{dt_today}_plus_by_time')
-plt.savefig(f'{dt_today}/scatter_matrix_color_plus_by_time.png')
-
 fig,ax=plt.subplots(figsize=(10,8))
 sns.heatmap(corr,vmax=1,vmin=-1,center=0) #各項目の相関をヒートマップで視覚的にわかりやすく
 plt.savefig(f'{dt_today}/corr-heatmap.png')
 
 
-#ヒストグラム作成
-x1 = data[data['plus_by_time'] > percentile[3]]['week_day']
-x2 = data[(data['plus_by_time'] <= percentile[3]) & (data['plus_by_time'] > percentile[2])]['week_day']
-x3 = data[(data['plus_by_time'] <= percentile[2]) & (data['plus_by_time'] > percentile[1])]['week_day']
-x4 = data[(data['plus_by_time'] <= percentile[1]) & (data['plus_by_time'] > percentile[0])]['week_day']
-#x5 = data[data['plus_by_time'] <= percentile[0]]['week_day']
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.hist([x1, x2, x3 , x4 ], bins=10, color=['blue', 'green', 'darkorange', 'red'], label=['Top 0~25%', 'Top 25~50%', 'Top 50~75%', 'Top 75~100%'], histtype='bar', stacked=True)
-ax.set_title('plus by time histogram')
-ax.set_xlabel('week day')
-ax.set_ylabel('freq')
-ax.legend(loc='upper left')
-plt.savefig(f'{dt_today}/hist_plus_by_time.png')
+for w in ['plus_followers', 'plus_by_viewers', 'plus_by_time']:
+    #四分位数でカラーわけ
+    percentile=np.percentile(data[w], q=[0, 25, 50, 75])
+    data.loc[data[w] > percentile[3] ,'color'] = 'blue'
+    data.loc[(data[w] <= percentile[3]) & (data[w] > percentile[2]) ,'color'] = 'green'
+    data.loc[(data[w] <= percentile[2]) & (data[w] > percentile[1]) ,'color'] = 'darkorange'
+    data.loc[(data[w] <= percentile[1]) ,'color'] = 'red'
+    #data.loc[data['plus_by_time'] <= percentile[0] ,'color'] = 'black'
+    color=data['color']
+    del data['color']
+    plt.figure()
+    plot=scatter_matrix(data,diagonal='density',color=color,figsize=(11,11))
+    plt.suptitle(f'{dt_today}_{w}')
+    plt.savefig(f'{dt_today}/scatter_matrix_color_{w}.png')
+    
+    for h in ['time_area', 'week_day']:
+        #ヒストグラム作成(曜日)
+        x1 = data[data[w] > percentile[3]][h]
+        x2 = data[(data[w] <= percentile[3]) & (data[w] > percentile[2])][h]
+        x3 = data[(data[w] <= percentile[2]) & (data[w] > percentile[1])][h]
+        x4 = data[(data[w] <= percentile[1])][h]
+        #x5 = data[data[w] <= percentile[0]][h]
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+        ax.hist([x1, x2, x3 , x4 ], bins=8 if h == 'time_area' else data[h].nunique(), color=['blue', 'green', 'darkorange', 'red'], label=['Top 0~25%', 'Top 25~50%', 'Top 50~75%', 'Top 75~100%'], histtype='bar', stacked=True)
+        ax.set_title(f'{dt_today} histogram {w} {h}')
+        ax.set_xlabel(h)
+        ax.set_ylabel('count')
+        ax.legend(loc='upper left')
+        plt.savefig(f'{dt_today}/hist_{w}_{h}.png')
+    time.sleep(2)
+
+print(data['time_area'].value_counts())
